d4428ab7e6ec42b1ea1ddf40bf23912cd7ded187
Williams.Silva <https://github.com/WillXXX/BeberAgua.git>
2021-12-10 17:57:30 -0300

    imprementações

--- app/src/main/java/com/example/beberagua/MainActivity.java
+++ app/src/main/java/com/example/beberagua/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.beberagua;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNotify;
+    private EditText edittext;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotify = findViewById(R.id.btn_notify);
+        edittext = findViewById(R.id.edit_text_number);
+        timePicker = findViewById(R.id.time_picker);
     }
+
 }
