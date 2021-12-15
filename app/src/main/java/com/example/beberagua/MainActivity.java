1dfd01ff6c11f10afd94bc6e19d3b429772825fb
Williams.Silva <https://github.com/WillXXX/BeberAgua.git>
2021-12-15 15:40:24 -0300

    Codigo para ver no Logcat

--- app/src/main/java/com/example/beberagua/MainActivity.java
+++ app/src/main/java/com/example/beberagua/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.beberagua;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import java.net.InterfaceAddress;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnNotify;
-    private EditText edittext;
+    private EditText editMinutes;
     private TimePicker timePicker;
+
+    private int hour;
+    private int minute;
+    private int interval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +27,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnNotify = findViewById(R.id.btn_notify);
-        edittext = findViewById(R.id.edit_text_number);
+        editMinutes = findViewById(R.id.edit_text_number);
         timePicker = findViewById(R.id.time_picker);
 
         timePicker.setIs24HourView(true);
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String sInterval = editMinutes.getText().toString();
+
+                hour = timePicker.getCurrentHour();
+                minute = timePicker.getCurrentMinute();
+                interval = Integer.parseInt(sInterval);
+
+                Log.d("Teste"," Hora: " + hour +" minute: " + minute + " intervalo: " + interval);
+            }
+        } );
     }
+
 
 }
