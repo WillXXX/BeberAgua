ee384e1c5241115e71f3d7f7a032ea5e2f9be771
Williams.Silva <https://github.com/WillXXX/BeberAgua.git>
2022-01-05 17:52:45 -0300

    Comentários

--- app/src/main/java/com/example/beberagua/MainActivity.java
+++ app/src/main/java/com/example/beberagua/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.beberagua;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ import java.net.InterfaceAddress;
 
 public class MainActivity extends AppCompatActivity {
 
+    //declaração de variaveies
     private Button btnNotify;
     private EditText editMinutes;
     private TimePicker timePicker;
@@ -22,40 +24,54 @@ public class MainActivity extends AppCompatActivity {
     private int minute;
     private int interval;
 
+    private boolean activated = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //retorno das funções
         btnNotify = findViewById(R.id.btn_notify);
         editMinutes = findViewById(R.id.edit_text_number);
         timePicker = findViewById(R.id.time_picker);
 
         timePicker.setIs24HourView(true);
-
-        //Evento de click com objeto anonimo
-        btnNotify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String sInterval = editMinutes.getText().toString();
-
-//Função para corrigir o erro de fechamento do botão notificar quando vazio
-
-                if (sInterval.isEmpty()) {
-//Toast serve para mostrar informações rápidas no Android
-                    Toast.makeText(getApplicationContext(), R.string.error_msg, Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                hour = timePicker.getCurrentHour();
-                minute = timePicker.getCurrentMinute();
-                interval = Integer.parseInt(sInterval);
-
-                Log.d("Teste", " Hora: " + hour + " minute: " + minute + " intervalo: " + interval);
-            }
-        });
     }
+
+    public void notifyClick(View view) {
+        editMinutes
+    }
+
+
+//        //Evento de click com objeto anonimo
+//        btnNotify.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                String sInterval = editMinutes.getText().toString();
+//
+////Função para corrigir o erro de fechamento do botão notificar quando vazio
+//
+//                if (sInterval.isEmpty()) {
+////Toast serve para mostrar informações rápidas no Android
+//                    Toast.makeText(getApplicationContext(), R.string.error_msg, Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//
+//                hour = timePicker.getCurrentHour();
+//                minute = timePicker.getCurrentMinute();
+//                interval = Integer.parseInt(sInterval);
+//
+//                btnNotify.setText(R.string.pause);
+//                int color;
+//                color = (ContextCompat.getColor(this, android.R.color.black);
+//                btnNotify.setBackgroundColor(color);
+//
+//                Log.d("Teste", " Hora: " + hour + " minute: " + minute + " intervalo: " + interval);
+//            }
+//        });
+//    }
 
 
 }
